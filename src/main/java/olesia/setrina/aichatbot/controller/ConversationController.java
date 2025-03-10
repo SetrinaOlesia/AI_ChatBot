3578b09ca442bdf92473988d796ab9a0cebe9209
SetrinaOlesia <setrina527@gmail.com>
2025-03-10 20:03:02 +0000

    T-04: added method addMessageToConversation

--- src/main/java/olesia/setrina/aichatbot/controller/ConversationController.java
+++ src/main/java/olesia/setrina/aichatbot/controller/ConversationController.java
@@ -1,14 +1,17 @@
 package olesia.setrina.aichatbot.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import olesia.setrina.aichatbot.conversations.ChatMessage;
 import olesia.setrina.aichatbot.conversations.Conversation;
 import olesia.setrina.aichatbot.repository.ConversationRepository;
 import olesia.setrina.aichatbot.repository.ProfileRepository;
@@ -27,17 +30,31 @@ public class ConversationController {
 
     @PostMapping("/conversations")
     public Conversation createNewConversation(@RequestBody CreateConversationRequest request) {
-        profileRepository.findById(request.profileId).orElseThrow(()
-            -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile not found"));
-        Conversation conversation = new Conversation(UUID.randomUUID().toString(), request.profileId(), new ArrayList<>());
+        profileRepository.findById(request.profileId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile not found" + request.profileId));
+        Conversation conversation = new Conversation(UUID.randomUUID()
+            .toString(), request.profileId(), new ArrayList<>());
         conversationRepository.save(conversation);
         return conversation;
     }
 
+    @PostMapping("/conversations/{conversationId}")
+    public Conversation addMessageToConversation(@PathVariable String conversationId, @RequestBody ChatMessage chatMessage) {
+        Conversation conversation = conversationRepository.findById(conversationId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Conversation message not found" + conversationId));
 
+        profileRepository.findById(chatMessage.authorId())
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found" + chatMessage.authorId()));
 
-    public record CreateConversationRequest(String profileId) {
+        ChatMessage messageWithTime = new ChatMessage(chatMessage.messageText(), chatMessage.authorId(), LocalDateTime.now());
+
+        conversation.messages().add(messageWithTime);
+        conversationRepository.save(conversation);
+        return conversation;
     }
 
+    public record CreateConversationRequest(String profileId) {
+
+    }
 
 }
